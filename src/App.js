afcab205e07b52de252291e704aef6cd54e81706
Francis Maina <sebastianfrancix@gmail.com>
2022-12-29 16:49:00 +0300

    added styling in our index.css file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Review from "./Review";
+
 function App() {
   return (
-    <div className="App">
-      <h2>Reviews App</h2>
-    </div>
+    <section className="container">
+      <div className="title">
+        <h2> Our Reviews</h2>
+        <div className="underline"></div>
+      </div>
+    </section>
   );
 }
 
